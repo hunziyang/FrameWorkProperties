0f25e1a46d8281b09dfbbf4b63661318cbc9d75e
Tommy <814230847@qq.com>
2021-06-28 22:31:40 +0800

    feat:增加拦截器

--- yang-module-web/src/main/java/com/yang/module/config/WebConfig.java
+++ yang-module-web/src/main/java/com/yang/module/config/WebConfig.java
@@ -1,13 +1,28 @@
 package com.yang.module.config;
 
+import com.yang.module.config.interceptor.InterceptorHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     /**
+     * 添加拦截器并指定
+     * 先过滤器再拦截器
+     *
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new InterceptorHandler()).addPathPatterns("/teacher/insert");
+    }
+
+    /**
      * 解决跨域问题
+     *
      * @param registry
      */
     @Override
@@ -19,5 +34,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .maxAge(3600)
                 .allowedHeaders("*");
     }
+
 
 }
---
+++ yang-module-web/src/main/java/com/yang/module/config/interceptor/InterceptorHandler.java
@@ -0,0 +1,29 @@
+package com.yang.module.config.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 拦截器
+ */
+@Slf4j
+public class InterceptorHandler implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("拦截器:");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
